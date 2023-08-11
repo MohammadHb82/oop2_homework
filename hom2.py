ef563560610f823c1c57afd6fbcbbf4af9eecc04
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-08-11 16:56:02 -0700

    Create hom2.py

---
+++ hom2.py
@@ -0,0 +1,47 @@
+# class car:
+#     def __init__(self,make,model,year,mileage,engine_type):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+#         self.mileage=mileage
+#         self.engine_type=engine_type
+#     def get_mileage(self,):
+#         return self.mileage
+    
+#     def set_mileage(self,new_Mileage):
+#         while new_Mileage<=self.mileage:
+#             print("New mileage should be greater than the current mileage")
+#             new_Mileage=int(input("enter new mileage __>"))
+#             self.meleage=new_Mileage
+#             print("mileage update sucsesfully")
+            
+        
+# my_car =car("bmW", "e200", 2022, 1000, "M10")
+# task1
+# Create a class called "Animal" with the following attributes: "name", "species", "age", and "weight". Create a method called "make_sound" that prints out the animal's sound. Add an if statement to the "make_sound" method to check if the animal is a mammal and print a different sound if it is.
+
+
+# class Animal:
+#     def __init__(self,name,species,age,weight):
+#         self.name=name
+#         self.species=species
+#         self.age=age
+#         self.weight=weight
+#     def make_sound(self,sound):
+
+#         #مش فاهم هاي الميثود
+#import math
+#class Rectangle:
+#    def __init__(self,width,height):
+#        self.width=width
+#        self.height=height
+#    def area(self):
+#        return self.height * self.width
+#    def perimeter(self):
+#        return 2 * (self.height + self.width)
+#accounts=Rectangle(5,4)
+#print(accounts.area())
+#task3
+        
+
+    
